30c16aeda35a5bce5e03977d8b4fc73379b3982c
Yevhenii <zvezdnev@gmail.com>
2017-07-29 19:42:28 +0300

    Initial commit

---
+++ src/config/whmcs.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	// API URL
+	'url'		=>	'http://url.com/whmcs/includes/api.php',
+	
+	// API USERNAME
+	'username'	=>	'admin_user',
+
+	// API PASSWORD
+	'password'	=>	'password123',
+
+    // API PASSWORD HASHED
+    'hashed_password' => false,
+	
+	// API RESPONSE TYPE
+	'response_type'	=> 'json', // json or xml
+];
